8dd5934b82c330b2ede10e9b8b52981fe59fe088
chandrakanth <Chandrakanth-Ganesan-Aiva>
2025-09-19 14:21:58 +0530

    19-09-2025

---
+++ DataTypes/DataTypes.js
@@ -0,0 +1,63 @@
+//1 Check type of a number.
+// let x = 10
+// console.log(typeof x);
+
+//--------------------------
+
+//2 Check type of a string.
+// let name='Chandrakanth'
+// console.log(typeof name);
+
+//----------------------------
+
+//3 Check type of a boolean.
+// let flag = true
+// console.log(typeof flag);
+
+//---------------------------
+
+//4 Check type of null
+// let n = null
+// console.log(typeof n);//object
+
+//---------------------------------
+
+//5 Check type of undefined
+// let u;
+// console.log(typeof u);
+
+//-------------------------
+
+//6 Check type of an array.
+// let arr = [1, 2, 3]
+// console.log(Array.isArray(arr));
+
+//-----------------------------------
+
+//7 Check type of an object.
+// let obj = { name: 'Chandru' }
+// console.log(typeof obj);
+
+//--------------------------------
+
+// 8 Concatenate string and number.
+// let a = 'age:'
+// let b = 23
+// console.log(a + b);
+
+// ------------------
+
+//9 Convert string "123" to number
+// let str = '123'
+// console.log(Number(str));
+
+// ---------------------------------
+// 10 Convert number 123 to string.
+// let num = 123;
+// console.log(String(num)); //here number and string will looks like same
+// let str = String(num)
+// console.log(typeof str);
+
+
+
+
---
+++ Variables/Variable.js
@@ -0,0 +1,70 @@
+// 1 Declare a variable x and assign value 10.
+// let x = 10
+// console.log(x)
+
+// ------------------
+
+//2 Declare a variable name and assign your name.
+// let name='Chandrakanth'
+// console.log(name);
+
+// ---------------------------
+
+//3 Declare a variable age using var and assign 23.
+// var age=23
+// console.log(age);
+
+// -------------------
+
+// 4 Declare a variable isStudent using let and assign true.
+// let isStudent = true
+// console.log(isStudent);
+
+// ------------------------
+
+// 5 Declare a variable without initializing, then assign value 5.
+// let num;
+// num=5
+// console.log(num);
+
+// ------------------
+
+// 6 Declare a variable score and increment it by 1.
+// let score=5
+// score++;
+// console.log(score);
+
+// --------------------
+
+//7 Declare a variable count and decrement it by 1.
+// let a = 5
+// a--;
+// console.log(a);
+
+//------------------
+
+// 8 Swap two variables a=5 and b=10.
+// let a = 5, b = 10;// If we initialize without ; , it will show error when we initialize like this
+// [a, b] = [b, a]
+// console.log(a, b);
+
+// -----------------
+
+//9 Declare multiple variables in one line (x=1, y=2, z=3).
+// let x = 1, y = 2, z = 3
+// console.log(x, y, z);
+
+// --------------------------
+
+// 10 Add two variables a=5 and b=7 and store in sum.
+// let a = 5, b = 7;
+// // let sum // It will be undefined
+// let sum = a + b
+// console.log(sum);
+
+
+
+
+
+
+
