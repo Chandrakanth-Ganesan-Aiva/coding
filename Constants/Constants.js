11b50ad46c687c2f7f2f2365c60fb92ca793898d
chandrakanth <Chandrakanth-Ganesan-Aiva>
2025-09-30 09:21:13 +0530

    Added Constants folder as normal files

---
+++ Constants/Constants.js
@@ -0,0 +1,26 @@
+//1 Declare a constant PI with value 3.14.
+// const PI=3.14
+// console.log(PI);
+
+//------------------
+
+// 2 Try changing value of a constant PI (should throw error).
+// const PI = 3.14
+// // PI=3.15 //If we try to change value declared in const it will show error
+// console.log(PI);
+
+// 3 Declare a constant NAME and print it.
+// const Name='Chandrakanth'
+// console.log(Name);
+
+//4 Declare multiple constants in one line (a=1, b=2).
+// const a = 1, b = 2 //Here if we not use ; , it will not show error
+// console.log(a, b);
+
+//5 Use constant in arithmetic: const TAX=0.05, calculate tax on 200.
+// const Tax = 0.05
+// let rate = 200
+// console.log(Tax * rate);
+
+
+
